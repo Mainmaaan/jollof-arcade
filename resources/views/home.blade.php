d6b66529593ab04e9538fa8628d9cbbbeb8942c1
Mainmaaan <sonsuuun@gmail.com>
2026-05-07 19:30:03 +0100

    fix routes for production

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,2 @@
-<h1 style="color:red;">WELCOME FILE ACTIVE</h1>
-
-<h1 style="color:red;">NEW HOME LOADED</h1>
+<h1>Welcome to Jollof Arcade 🎮</h1>
+<a href="/game/snake">Play Snake</a>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/game/{name}', function ($name) {
-    return view('game', ['name' => $name]);
+    return view('game', compact('name'));
 });
