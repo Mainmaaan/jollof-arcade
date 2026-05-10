3f113bbca4b0a99f23bff10ec6fbc9d4101e9733
Mainmaaan <sonsuuun@gmail.com>
2026-05-10 10:48:42 +0100

    Force file session fix for Render

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,11 @@
-<?phpRoute::get('/game/{name}', function ($name) {
-    return "GAME PAGE WORKING: " . $name;
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/game/{name}', function ($name) {
+    return view('game', compact('name'));
 });
